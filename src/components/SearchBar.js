7aad733d3f92d39f8f520924adb4dc8508c19e9f
Eder <chlrobert93@gmail.com>
2021-07-22 13:46:43 -0500

    Refactorización de la barra de búsqueda

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-class SearchBar extends React.Component{
-   state = { term: ''};
+const SearchBar = ({onFormSubmit})  =>{
+    const [term, setTerm] = useState('');
 
-   onInpiutChange =  event => {
-       this.setState({term: event.target.value});
-
-   };   
-
-   onFormSubmit = event => {
-        event.preventDefault();
-
-        //Devolución de llamada de componente principal
-        //llamarcon state para informar cual es el temino de búsqueda
-         this.props.onFormSubmit(this.state.term);
-   }
-
-    render(){
-        return (
-        
-            <div className="colorPage search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input type="text" value={this.state.term} onChange={this.onInpiutChange} />
-                    </div>
-                </form>
-            </div>
+    /*
+     const  onInpiutChange =  event => {
+        //this.setState({term: event.target.value});
+        setTerm(event.target.value);
+    };
+    */   
  
-        );
-            
-    }
+     const onSubmit = (event) => {
+         event.preventDefault();
+ 
+         //Devolución de llamada de componente principal
+         //llamarcon state para informar cual es el temino de búsqueda
+          onFormSubmit(term);
+          //this.props.onFormSubmit(this.state.term);
+    };
 
-}
+    return (
+        
+        <div className="colorPage search-bar ui segment">
+            <form onSubmit={onSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text" value={term} onChange={(event) => setTerm(event.target.value)} />
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default SearchBar;
